4ab6f6e538f706b81449f03ab6353b63e557a826
ZA-Nahid <nahid.382.bubt@gmail.com>
2017-11-19 14:04:13 +0600

    Add files via upload

---
+++ 1.7 exercise 3.cpp
@@ -0,0 +1,43 @@
+/*1.7 exercise 3*/
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<math.h>
+
+using namespace std;
+
+int min(int a,int b)
+{
+   if(a>b)
+   {
+      return a;
+   }
+   else return b;
+}
+
+char min(char a,char b)
+{
+   if(a>b)
+   {
+      return a;
+   }
+   else return b;
+}
+
+double min(double a,double b)
+{
+   if(a>b)
+   {
+      return a;
+   }
+   else return b;
+}
+
+
+int main()
+{
+cout<<min(1000,200)<<endl;
+cout<<min("A","B")<<endl;
+cout<<min(10.33,2.33)<<endl;
+return 0;
+}
