d209324bf5b89d80d7fa4b72598895eedf86aac5
ZA-Nahid <nahid.382.bubt@gmail.com>
2017-11-19 13:52:46 +0600

    Add files via upload

---
+++ 1.7 exercise 1.cpp
@@ -0,0 +1,31 @@
+/*1.7 exercise 1*/
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<math.h>
+
+using namespace std;
+
+int sroot(int a)
+{
+   return sqrt(a);
+}
+
+long int sroot(long int a)
+{
+   return sqrt(a);
+}
+
+double sroot(double a)
+{
+   return sqrt(a);
+}
+
+
+int main()
+{
+cout<<sroot(100)<<endl;
+cout<<sroot(10035465)<<endl;
+cout<<sroot(100.365)<<endl;
+return 0;
+}
